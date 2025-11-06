e45f2435304bdde11733bb37dcda6126bdf95a6e
Kseniia Shaikhvalieva <commerc-ksyu@mail.ru>
2025-11-06 20:50:32 +0300

    fix type errors reported by mypy

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,12 @@
-from typing import List, Dict, Any
+from typing import Any, Dict, List
 
-def filter_by_state(operations: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
-    return [op for op in operations if op.get('state') == state]
+
+def filter_by_state(operations: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    return [op for op in operations if op.get("state") == state]
 
 
 def sort_by_date(operations: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """
     Сортирует список словарей по ключу date.
     """
-    return sorted(operations, key=lambda x: x.get('date'), reverse=reverse)
+    return sorted(operations, key=lambda x: x.get("date", "1970-01-01T00:00:00.000000"), reverse=reverse)
