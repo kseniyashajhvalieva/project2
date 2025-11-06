1829b593cb7303dfde74b669f9e76536e8ac6f6d
Kseniia Shaikhvalieva <commerc-ksyu@mail.ru>
2025-11-06 19:13:40 +0300

    add sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -2,3 +2,10 @@ from typing import List, Dict, Any
 
 def filter_by_state(operations: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
     return [op for op in operations if op.get('state') == state]
+
+
+def sort_by_date(operations: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+    """
+    Сортирует список словарей по ключу date.
+    """
+    return sorted(operations, key=lambda x: x.get('date'), reverse=reverse)
