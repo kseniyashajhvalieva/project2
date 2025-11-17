f6b9b5310a03b5085c0a053936abb9caf3863f75
Kseniia Shaikhvalieva <commerc-ksyu@mail.ru>
2025-11-17 19:58:43 +0300

    Генератор: возвращает описание операций по очереди

--- src/generators.py
+++ src/generators.py
@@ -6,3 +6,11 @@ def filter_by_currency(transactions: List[Dict[str, Any]], currency: str) -> Ite
     for transact in transactions:
         if transact.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield transact
+
+
+def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Iterator[str]:
+    """Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+    for transact in transactions:
+        result_description = transact.get("description")
+        if result_description != None:
+            yield result_description
