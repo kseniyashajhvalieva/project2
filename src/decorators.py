bb18aa224277f725c0a5f8f301e1205fc2a79697
Kseniia Shaikhvalieva <commerc-ksyu@mail.ru>
2025-11-24 19:12:14 +0300

    Реализован декоратор log.

---
+++ src/decorators.py
@@ -0,0 +1,29 @@
+from functools import wraps
+from typing import Callable, Any
+
+
+def log(filename: str ="") -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            log_message = ""
+            func_result = None
+
+            try:
+                func_result = func(*args, **kwargs)
+                log_message = f"{func.__name__} ok"
+            except Exception as e:
+                log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+
+            if filename:
+                with open(filename, 'a') as f:
+                    f.write(log_message + '\n')
+            else:
+                print(log_message)
+
+            if "error" in log_message:
+                raise
+
+            return func_result
+        return wrapper
+    return decorator
