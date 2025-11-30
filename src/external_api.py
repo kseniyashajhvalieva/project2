d76404bd051737eed67d792d67cf0b0198d62a19
Kseniia Shaikhvalieva <commerc-ksyu@mail.ru>
2025-11-30 16:29:25 +0300

    Запущены линтеры и форматеры для новых функций

--- src/external_api.py
+++ src/external_api.py
@@ -1,11 +1,13 @@
 import os
+
 import requests
 from dotenv import load_dotenv
 
-load_dotenv() # Загружаем переменные из .env
+load_dotenv()  # Загружаем переменные из .env
 
 API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
 BASE_URL = "https://api.apilayer.com/exchangerates_data"
+
 
 def convert_currency(transaction: dict) -> float:
     """
@@ -23,7 +25,7 @@ def convert_currency(transaction: dict) -> float:
 
     try:
         response = requests.get(f"{BASE_URL}/convert", headers=headers, params=params)
-        response.raise_for_status() # Вызывает исключение для плохих статусов
+        response.raise_for_status()  # Вызывает исключение для плохих статусов
         data = response.json()
         if data.get("success"):
             return float(data["result"])
@@ -32,4 +34,4 @@ def convert_currency(transaction: dict) -> float:
     except (KeyError, TypeError) as e:
         print(f"Ошибка обработки данных API: {e}")
 
-    return float(amount) # В случае ошибки возвращаем исходную сумму
+    return float(amount)  # В случае ошибки возвращаем исходную сумму
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 import json
 
+
 def read_json_file(file_path: str) -> list:
     """Читает JSON-файл и возвращает список операций."""
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
             if isinstance(data, list):
                 return data
