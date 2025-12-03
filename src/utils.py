27550479761e1c01bb9848996bba6a0bf4a28a4e
Kseniia Shaikhvalieva <commerc-ksyu@mail.ru>
2025-12-03 19:59:54 +0300

    Настроен логер для utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('logs/utils.log', mode='w')
+logger.addHandler(file_handler)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
 
 
 def read_json_file(file_path: str) -> list:
