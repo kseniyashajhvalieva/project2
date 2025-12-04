13415c8891c66c2108b185b658ab8a49d5175742
Kseniia Shaikhvalieva <commerc-ksyu@mail.ru>
2025-12-04 18:31:06 +0300

    Добавлено логгирование для masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,16 @@
 import logging
+import os
 
-logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler('logs/masks.log', mode='w')
-logger.addHandler(file_handler)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-logger.setLevel(logging.DEBUG)
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+logger_masks = logging.getLogger(__name__)
+file_handler_masks = logging.FileHandler('logs/masks.log', mode='w')
+logger_masks.addHandler(file_handler_masks)
+formatter_masks = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler_masks.setFormatter(formatter_masks)
+logger_masks.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(card_number: int) -> str:
@@ -19,10 +24,20 @@ def get_mask_card_number(card_number: int) -> str:
         Замаскированный номер карты в формате XXXX XX** **** XXXX.
     """
     if not isinstance(card_number, int):
-        raise TypeError("Card number must be an integer.")
+        logger_masks.error(
+            f"В функцию get_mask_card_number передан нечисловой аргумент: {type(card_number)}."
+        )
+        raise TypeError("Номер карты должен быть числом.")
 
     card_number_str = str(card_number)
+    if not card_number_str.isdigit() or len(card_number_str) != 16:
+        logger_masks.error(
+            f"Некорректный номер карты: '{card_number_str}'. Ожидается 16 цифр."
+        )
+        raise ValueError("Номер карты должен содержать 16 цифр.")
+
     masked_number = f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
+    logger_masks.info(f"Номер карты успешно замаскирован: '{masked_number}'")
     return masked_number
 
 
@@ -37,7 +52,13 @@ def get_mask_account(account_number: int) -> str:
         Замаскированный номер счета в формате **XXXX.
     """
     if not isinstance(account_number, int):
-        raise TypeError("Account number must be an integer.")
+        logger_masks.error(
+            f"В функцию get_mask_account передан нечисловой аргумент: {type(account_number)}."
+        )
+        raise TypeError("Номер счета должен быть числом.")
 
     account_number_str = str(account_number)
-    return f"**{account_number_str[-4:]}"
+    masked_number = f"**{account_number_str[-4:]}"
+    logger_masks.info(f"Номер счета успешно замаскирован: '{masked_number}'")
+
+    return masked_number
