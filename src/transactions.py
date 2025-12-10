1b6fb4ed28c75a02d15d26fe6bac9465b696d124
Kseniia Shaikhvalieva <commerc-ksyu@mail.ru>
2025-12-10 20:29:44 +0300

    Добавлены Docstrings и аннотации типов

--- src/transactions.py
+++ src/transactions.py
@@ -1,10 +1,13 @@
 import pandas as pd
+from typing import List, Dict, Any
 
-def csv_transactions(file_path):
+def csv_transactions(file_path: str) -> List[Dict[str, Any]]:
+    """ Считывает финансовые операции из CSV"""
     df = pd.read_csv(file_path)
     return df.to_dict('records')
 
 
-def excel_transactions(file_path):
+def excel_transactions(file_path: str) -> List[Dict[str, Any]]:
+    """ Считывает финансовые операции из Excel."""
     df = pd.read_excel(file_path)
     return df.to_dict('records')
