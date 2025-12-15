4509a47f712d5022517b90b9037db401cbf99ffa
Kseniia Shaikhvalieva <commerc-ksyu@mail.ru>
2025-12-15 19:28:54 +0300

    Добавлена функция, которая считает операции по категориям

--- src/search.py
+++ src/search.py
@@ -1,5 +1,5 @@
 import re
-
+from collections import Counter
 from typing import List, Dict, Any
 
 
@@ -7,3 +7,10 @@ def search_by_description(data: List[Dict[str, Any]], search_str: str) -> List[D
     """Ищет операции по строке в описании с использованием регулярных выражений."""
     pattern = re.compile(re.escape(search_str), re.IGNORECASE)
     return [op for op in data if op.get("description") and pattern.search(op["description"])]
+
+
+def count_by_categories(data: List[Dict[str, Any]], categories: List[str]) -> Dict[str, int]:
+    """Считает операции по категориям на основе поля description."""
+    descriptions = [op.get("description", "") for op in data]
+    category_counts = Counter(desc for desc in descriptions if desc in categories)
+    return dict(category_counts)
